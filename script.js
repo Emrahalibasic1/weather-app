8834c63240806c3a12f78933d9c972a9ea7fdeff
Emrahalibasic1 <alibasicemrah98@gmail.com>
2024-01-22 18:09:57 +0100

    added public ip with json

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const temp = document.getElementById("temp");
+const temp = document.getElementById("temp"),
+        date = document.getElementById("date-time");
 
-
+// Get the current time
 let currentCity = "";
 let currentUnit = "";
-let hurlyorWeek = "Week";
+let hourlyWeek = "Week";
 
 
 // Update time 
@@ -13,6 +14,7 @@ function getDataTime() {
     hour = now.getHours(),
     minute = now.getMinutes();
 
+// An array with the names of the days of the week
 
     let days = [
         'Sunday',
@@ -24,14 +26,47 @@ function getDataTime() {
         'Saturday'
     ];
 
+// Format the clock as 12-hour format
     hour = hour % 12;
     if(hour < 10){
         hour = "0" + hour;
     }
+    // Add a zero in front of single-digit minutes
     else if(minute < 10){
         minute = "0" + minute;
     }
 
+// Get the name of the day of the week  
     let dayString = days[now.getDay()]
+
+    
+// returning days hours and minutes
     return `${dayString},${hour}:${minute}`;
 }
+
+date.innerText = getDataTime();
+
+
+// update time every second
+
+setInterval(() => {
+    date.innerText = getDataTime();
+}, 1000);
+
+// function to get public ip with fetch
+
+function getPublicIp(){
+    fetch("https://geolocation-db.com/json/",{
+        method: "GET",
+    } ) 
+    .then((Response) => Response.json())
+    .then((data) =>{
+        console.log(data)
+        currentCity = data.currentCity;
+    });
+}
+
+getPublicIp();
+
+//function to get weather data  
+ 
